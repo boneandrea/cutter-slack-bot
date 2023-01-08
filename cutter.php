4197c63319504db8736f28cc01b48d62a3185435
Ubuntu <ubuntu@ip-172-26-7-226.ap-northeast-1.compute.internal>
2023-01-08 14:32:01 +0900

    refactor

--- cutter.php
+++ cutter.php
@@ -5,97 +5,32 @@ ini_set('xdebug.var_display_max_data', -1);
 ini_set('xdebug.var_display_max_depth', -1);
 
 define("TOKEN_FILE", "access_token.json");
-define("CLIENT_ID", "405980369974.4586316536706");
-define("CLIENT_SECRET", "9ee2eefaa26906bb89c44f058b0a2c3c");
 define("BOT_SELF_USERID", "U04HY33JT9N");
 define("API_ROOT", "https://slack.com/api");
 
 require_once("util.php");
+require_once("src/slack.php");
 require("NgWord.php");
 
 class CutterBot
 {
-    private $token;
+    private $slack;
+
     public function __construct()
     {
-        $this->token=json_decode(file_get_contents(TOKEN_FILE), true);
-    }
-
-    public function slack($message, $channel, $token, $thread_ts)
-    {
-        $data = [
-            "token" => $token,
-            "channel" => $channel,
-            "text" => $message,
-            "username" => "MySlackBot",
-            "thread_ts" => $thread_ts,
-        ];
-        l($data);
-        return $this->http_post("/chat.postMessage", $data, image:false);
-    }
-
-    public function slack_image($message, $channel, $token, $thread_ts)
-    {
-        $cfile = new CURLFile(dirname(__FILE__)."/image/kurosawasan.jpg", 'image/jpeg', 'api_image.jpg');
-        $data = [
-            "token" => $token,
-            "channels" => $channel,
-            "text" => $message,
-            "username" => "MySlackBot",
-            "thread_ts" => $thread_ts,
-            'initial_comment'=>$message,
-            "file"=>$cfile,
-        ];
-
-        return $this->http_post("/files.upload", $data, image: true);
-    }
-
-    public function renewToken(string $token)
-    {
-        $data = [
-            "refresh_token" => $token,
-            "client_id"=>CLIENT_ID,
-            "client_secret"=>CLIENT_SECRET,
-            "grant_type"=>"refresh_token",
-        ];
-
-        $r=$this->http_post("/oauth.v2.access", $data);
-
-        // renew token
-        file_put_contents(TOKEN_FILE, $r);
-        $this->token=json_decode(file_get_contents(TOKEN_FILE), true);
-    }
-
-    public function http_post($apiUrl, $data, $image=false)
-    {
-        $ch = curl_init(API_ROOT.$apiUrl);
-        l($image);
-        if ($image) {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: multipart/form-data']);
-        } else {
-            $data = http_build_query($data);
-        }
-        l($data);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        return $result;
+        $this->slack=new Slack();
     }
 
     public function send($thread_ts, $message="オッスmunou")
     {
-        $r=$this->slack($message, "#general", $this->token["access_token"], $thread_ts);
+        $r=$this->slack->slack($message, "#general", $thread_ts);
         l($result=json_decode($r, true));
         return $result;
     }
 
     public function send_image($thread_ts, $message="オッスmunou")
     {
-        $r=$this->slack_image($message, "#general", $this->token["access_token"], $thread_ts);
+        $r=$this->slack->slack_image($message, "#general", $thread_ts);
         l($result=json_decode($r, true));
         return $result;
     }
@@ -136,7 +71,7 @@ class CutterBot
 
             if (($r["error"] ?? "") === "token_expired") {
                 l("renew token.");
-                $this->renewToken($this->token["refresh_token"]);
+                $this->slack->renewToken();
                 l("send again.");
                 $this->send($thread_ts);
             }
---
+++ src/slack.php
@@ -0,0 +1,78 @@
+<?php
+
+define("TOKEN_FILE", "../access_token.json");
+define("CLIENT_ID", "405980369974.4586316536706");
+define("CLIENT_SECRET", "9ee2eefaa26906bb89c44f058b0a2c3c");
+define("API_ROOT", "https://slack.com/api");
+
+class Slack
+{
+    private $token;
+    public function __construct()
+    {
+        $this->token=json_decode(file_get_contents(TOKEN_FILE), true);
+    }
+
+    public function slack($message, $channel, $thread_ts)
+    {
+        $data = [
+            "token" => $this->token["access_token"],
+            "channel" => $channel,
+            "text" => $message,
+            "username" => "MySlackBot",
+            "thread_ts" => $thread_ts,
+        ];
+        l($data);
+        return $this->http_post("/chat.postMessage", $data, image:false);
+    }
+
+    public function slack_image($message, $channel, $thread_ts)
+    {
+        $cfile = new CURLFile(dirname(__FILE__)."/../image/kurosawasan.jpg", 'image/jpeg', 'api_image.jpg');
+        $data = [
+            "token" => $this->token["access_token"],
+            "channels" => $channel,
+            "text" => $message,
+            "username" => "MySlackBot",
+            "thread_ts" => $thread_ts,
+            'initial_comment'=>$message,
+            "file"=>$cfile,
+        ];
+
+        return $this->http_post("/files.upload", $data, image: true);
+    }
+
+    public function renewToken()
+    {
+        $data = [
+            "refresh_token" => $this->token["refresh_token"],
+            "client_id"=>CLIENT_ID,
+            "client_secret"=>CLIENT_SECRET,
+            "grant_type"=>"refresh_token",
+        ];
+
+        $r=$this->http_post("/oauth.v2.access", $data);
+
+        // renew token
+        file_put_contents(TOKEN_FILE, $r);
+        $this->token=json_decode(file_get_contents(TOKEN_FILE), true);
+    }
+
+    public function http_post($apiUrl, $data, $image=false)
+    {
+        $ch = curl_init(API_ROOT.$apiUrl);
+        if ($image) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: multipart/form-data']);
+        } else {
+            $data = http_build_query($data);
+        }
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+    }
+}
