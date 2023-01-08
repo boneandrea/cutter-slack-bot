6f6567b901b57252a44dd54ce932b26c4ffe8bd9
Ubuntu <ubuntu@ip-172-26-7-226.ap-northeast-1.compute.internal>
2023-01-09 00:51:54 +0900

    refactor

---
+++ cutter_bot.php
@@ -0,0 +1,81 @@
+z<?php
+
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_data', -1);
+ini_set('xdebug.var_display_max_depth', -1);
+
+define("BOT_SELF_USERID", "U04HY33JT9N");
+
+require_once("src/util.php");
+require_once("src/slack.php");
+require_once("src/NgWord.php");
+require_once("src/resolver.php");
+require_once("src/god.php");
+require_once("src/kurosawa.php");
+require_once("src/munou.php");
+
+class CutterBot
+{
+    private $slack;
+
+    public function __construct()
+    {
+        $this->slack=new Slack();
+    }
+
+    // for verifying endpoint
+    public function verify_response(array $json)
+    {
+        $response=[
+            "token"=>$json["token"],
+            "challenge"=>$json["challenge"],
+            "type"=>"url_verification"
+        ];
+
+        header("Content-Type: application/json; charset=UTF-8");
+        echo json_encode($response);
+        exit;
+    }
+
+    /**
+     * 適切に応答する
+     * access_tokenが切れていればrenewする
+     *
+     */
+    public function handleMessage(array $json)
+    {
+        l($event=$json["event"]);
+        $user=$event["user"] ?? "";
+        $text=$event["text"] ?? "";
+        $thread_ts=$event["thread_ts"] ?? "";
+
+        if ($user === BOT_SELF_USERID) {
+            l("bot-self message");
+            return;
+        }
+
+        $resolver=new Resolver($text);
+        $resolver->add(new Kurosawa());
+        $resolver->add(new NgWord());
+        $resolver->add(new Munou());
+        $resolver->add(new God());
+
+        if (($action=$resolver->resolve()) === null) {
+            return;
+        }
+
+        $r=$action->perform($this->slack, $thread_ts);
+
+        if ($r["ok"] ?? "") {
+            return;
+        }
+
+        l($r);
+        if (($r["error"] ?? "") === "token_expired") {
+            l("renew token.");
+            $this->slack->renewToken();
+            $r=$action->perform($this->slack, $thread_ts);
+            l("sent again.");
+        }
+    }
+}
--- src/NgWord.php
+++ src/NgWord.php
@@ -4,16 +4,17 @@ require_once("Iperform.php");
 
 class NgWord implements Iperform
 {
-    public const FILENAME="ng_words.txt";
+    public const NGWORD_FILENAME="ng_words.txt";
+    public const KUWATA_FILENAME="kuwata.txt";
     private $words=[];
-    private $count;
+    private $matched;
 
     public function test(string $text)
     {
         $ngwords=$this->read_ngwords();
-        $matched=array_filter($ngwords, fn ($w) =>preg_match("/".$w."/s", $text));
+        $this->matched=array_filter($ngwords, fn ($w) =>preg_match("/".$w."/s", $text));
 
-        return ($this->count=count($matched)) > 0;
+        return count($this->matched) > 0;
     }
 
     public function perform($slack, string $thread_ts)
@@ -23,7 +24,7 @@ class NgWord implements Iperform
         return $slack->send(
             "#general",
             $thread_ts,
-            $this->count.
+            count($this->matched).
             "個のNGワードがあったのや。".
             $kuwata[array_rand($kuwata, 1)]
         );
@@ -31,14 +32,14 @@ class NgWord implements Iperform
 
     public function read_ngwords()
     {
-        $words=file_get_contents(self::FILENAME);
+        $words=file_get_contents(self::NGWORD_FILENAME);
         $words=explode("\n", $words);
         $words=array_filter($words);
         return array_map(fn ($e) =>trim($e), $words);
     }
     public function read_kuwata(): array
     {
-        $kuwata=file_get_contents("kuwata.txt");
+        $kuwata=file_get_contents(self::KUWATA_FILENAME);
         $kuwata=explode("\n", $kuwata);
         $kuwata=array_filter($kuwata);
         return array_map(fn ($e) =>trim($e), $kuwata);
