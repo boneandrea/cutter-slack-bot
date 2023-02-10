63e4cf5f4958d1b6c6107262f59f3d87481b5982
Ubuntu <ubuntu@ip-172-26-7-226.ap-northeast-1.compute.internal>
2023-02-10 17:39:20 +0900

    Event subscribe 用のchallenge-response関数追加

--- cutter.php
+++ cutter.php
@@ -12,8 +12,28 @@ ini_set('xdebug.var_display_max_depth', -1);
 
 require_once("src/util.php");
 
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    exit;
+}
+
+function return_challenge($json)
+{
+    header("Content-Type: application/json; charset=utf-8");
+    echo json_encode($json);
+    exit;
+}
+
 try {
+    $app_auth=false;
     $json=json_decode(file_get_contents("php://input") ?? "{}", true);
+
+    // 認証時有効にする
+    // Event Subscriptions
+    if ($app_auth) {
+        l($json["challenge"]);
+        return_challenge($json);
+    }
+
     $x=new CutterBot();
     $x->handleMessage($json);
 } catch(Exception $e) {
