a05157694c4d189d5cb3a0653197c6e1ea515fd8
Ubuntu <ubuntu@ip-172-26-7-226.ap-northeast-1.compute.internal>
2023-03-18 16:29:39 +0900

    神をランダムに

--- src/Action/God.php
+++ src/Action/God.php
@@ -13,12 +13,15 @@ class God implements IPerform
 
     public function perform($slack, string $thread_ts)
     {
+        $images=["god.jpg", "god2.jpg"];
+        $image=$images[mt_rand(0, count($images)-1)];
+
         return $slack->send_image(
             message: "そうでしゅねぇ〜",
             channels: "#general",
             thread_ts: $thread_ts,
             alt_text: "唯一神.jpg",
-            image:"god.jpg"
+            image:$image
         );
     }
 }
